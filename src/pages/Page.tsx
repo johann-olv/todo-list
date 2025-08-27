9309dbd92683364a28f8335cce85257abd1deab2
Johann Olv <johann.olv@gmail.com>
2025-08-27 18:42:43 -0300

    ajustes no css

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,6 @@
 "use react";
 import { TodoContext, TodoProvider } from "@/contexts/TodoContext";
-import { Trash2, Circle, CircleCheckBig, CirclePlus } from "lucide-react";
+import { Trash2, Square, SquareCheck, CirclePlus, Moon,Sun } from "lucide-react";
 import { useContext, useState } from "react";
 type Task = {
   id: number;
@@ -24,6 +24,7 @@ export const Page = () => {
         completed: false,
       },
     });
+    setInputTask("");
   };
 
   const handleRemoveTask = (id: number) => {
@@ -45,63 +46,83 @@ export const Page = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#0D1117]">
-      <div className="w-full max-w-md flex flex-col gap-4 items-center rounded-2xl bg-[#161B22] p-15 shadow-lg">
-        <h1 className="text-[#C9D1D9] text-2xl mb-1">Lista de Tarefas</h1>
+    <div className="flex max-w-screen min-h-screen justify-center bg-[#0D1117]">
+      <div className="mt-10 mb-10 w-full max-w-md flex flex-col gap-4 items-center rounded-2xl bg-[#161B22] p-15 shadow-lg">
+        <h1 className="text-[#C9D1D9] text-3xl ">Os Trampos de hoje!</h1>
+        <p className="text-gray-400 italic mb-1">25 novembro de 2025 - 18:33</p>
         {/* INPUT DE ADICIONAR TODOS */}
         <div className="w-full flex justify-between gap-5">
           <input
             type="text"
-            className="rounded-2xl bg-[#21262D] text-[#FAF9F6] p-4"
+            className="rounded-2xl bg-[#21262D] text-white-100 p-4"
             placeholder="Escreva sua tarefa"
+            
+            value={inputTask}
             onChange={(e) => setInputTask(e.target.value)}
           />
           <button
             onClick={handleAddTask}
-            className="cursor-pointer rounded-2xl bg-[#3FB950] p-4 text-sm text-black shadow hover:bg-white"
+            disabled={inputTask.trim() === ""}
+           className={`p-3 rounded-xl text-white transition 
+            ${
+              inputTask.trim() === ""
+                ? "bg-gray-600 cursor-not-allowed"
+                : "bg-blue-500 hover:bg-blue-600"
+            }`}
           >
             <CirclePlus size={30} />
           </button>
         </div>
         {/* LISTA DE TODOS */}
-        {todoCtx?.todos.map((item) => (
-          <div key={item.id} className="bg-[#21262D] w-full p-4 rounded-2xl ">
-            <div className="flex items-center justify-between gap-10 text-black">
+        {todoCtx?.todos.length === 0 ? (
+          <p className="text-gray-400 italic">Nenhuma tarefa ainda!</p>
+        ) : (
+          <>
+            {todoCtx?.todos.map((item) => (
               <div
-                onClick={() => handleToggleTask(item.id)}
-                className="flex items-center gap-2 text-lg "
+                key={item.id}
+                className="bg-[#21262D] w-full p-4 rounded-2xl "
               >
-                {item.completed ? (
-                  <>
-                    <CircleCheckBig
-                      className="cursor-pointer flex-shrink-0"
-                      color="#57cc99"
-                      size={30}
-                    />
-                    <h1 className="break-all line-through text-[#FAF9F6]">
-                      {item.text}
-                    </h1>
-                  </>
-                ) : (
-                  <>
-                    <Circle
-                      className="cursor-pointer flex-shrink-0"
-                      color="#FAF9F6"
-                      size={30}
-                    />
-                    <h1 className="break-all text-[#FAF9F6]">{item.text}</h1>
-                  </>
-                )}
+                <div className="flex items-center justify-between gap-10 text-black">
+                  <div
+                    onClick={() => handleToggleTask(item.id)}
+                    className="flex items-center gap-2 text-lg "
+                  >
+                    {item.completed ? (
+                      <>
+                        <SquareCheck
+                          className="cursor-pointer flex-shrink-0"
+                          color="#57cc99"
+                          size={30}
+                        />
+                        <h1 className="break-all line-through text-white-100">
+                          {item.text}
+                        </h1>
+                      </>
+                    ) : (
+                      <>
+                        <Square
+                          className="cursor-pointer flex-shrink-0"
+                          color="#FAF9F6"
+                          size={30}
+                        />
+                        <h1 className="break-all text-white-100">
+                          {item.text}
+                        </h1>
+                      </>
+                    )}
+                  </div>
+                  <button
+                    onClick={() => handleRemoveTask(item.id)}
+                    className="cursor-pointer"
+                  >
+                    <Trash2 color="#F85149" size={30} />
+                  </button>
+                </div>
               </div>
-              <button
-                onClick={() => handleRemoveTask(item.id)}
-                className="cursor-pointer"
-              >
-                <Trash2 color="#F85149" size={30} />
-              </button>
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
